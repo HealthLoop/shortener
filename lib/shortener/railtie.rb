f038348b55bda6fd9550a770b7b5069ae2181968
thib <thibault.poisson@gmail.com>
2016-07-19 15:55:35 -0700

    Do not autoload the Shortener::ActiveRecordExtension

--- lib/shortener/railtie.rb
+++ lib/shortener/railtie.rb
@@ -3,9 +3,10 @@ require "shortener"
 
 class Shortener::Railtie < ::Rails::Railtie #:nodoc:
   initializer "shortener.register.active.record.extension" do
-    ActiveSupport.on_load :active_record do
-      extend Shortener::ActiveRecordExtension
-    end
+    # This way of extending ActiveSupport breaks schema plus and makara; removing the autoload for now
+    #ActiveSupport.on_load :active_record do
+    #  extend Shortener::ActiveRecordExtension
+    #end
     ActiveSupport.on_load :action_view do
       include Shortener::ShortenerHelper
     end
